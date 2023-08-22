c081d8add42a513fcde06eb1ae3564016cbd1925
PetcuBogdan <bogdan.petcu02@e-uvt.ro>
2023-08-22 21:47:13 +0300

    modificari parola

--- djangoAuth/authapi/settings.py
+++ djangoAuth/authapi/settings.py
@@ -61,7 +61,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = '10b.petcu.bogdan@gmail.com'
-EMAIL_HOST_PASSWORD = 'sudaevnronlfmfdr'
+EMAIL_HOST_PASSWORD = 'sudaevnronlfmfdra' #sterge a de la final ca sa functioneze
 EMAIL_USE_TLS = True
 
 SITE_DOMAIN = "http://localhost:3000"
